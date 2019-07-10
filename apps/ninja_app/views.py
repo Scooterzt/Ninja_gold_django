85ce79e522a58c13e4f065cd997c17c4fb6ce9c4
Roman <Scooterzt@gmail.com>
2019-07-09 19:35:53 -0700

    ninja_gold add activities

--- apps/ninja_app/views.py
+++ apps/ninja_app/views.py
@@ -3,21 +3,29 @@ import random
 
 def index(request):
     if "your_gold" not in request.session:
-        request.session["your_gold"] = 0 
+        request.session["your_gold"] = 0
+    if "messages" not in request.session:
+        request.session["messages"] = []
     return render(request,"ninja_app/index.html")
 
 def process_money(request):
     location = request.POST["location"]
     if location == "farm":
-        request.session["your_gold"]+= random.randint(10,20) 
+        your_gold = random.randint(10,20) 
     elif location == "cave":
-        request.session["your_gold"] += random.randint(5,10)
+        your_gold = random.randint(5,10)
     elif location == "house":
-        request.session["your_gold"] += random.randint(2,5)
+        your_gold = random.randint(2,5)
     else:
-        request.session["your_gold"] += random.randint(-50,50)
-    request.session["location"] = location
+        your_gold = random.randint(-50,50)
+
+    if your_gold > 0:
+        request.session["messages"].insert(0, f"<p>Earned {str(your_gold)} gold from the {location}</p>")
+    else:
+        request.session["messages"].insert(0, f"<p>Entered a {location} and lost {str(your_gold)} gold...</p>")
+    request.session["your_gold"] += your_gold
     return redirect("/")
 def reset(request):
     request.session.pop("your_gold")
+    request.session.pop("messages")
     return redirect("/")
